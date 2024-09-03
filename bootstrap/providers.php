420a37e1953ab8fc4a0dde1fd49ca09c6bfac2e7
sleepzc0de <auliyaputraazhari@gmail.com>
2024-09-03 16:16:55 +0700

    first

---
+++ bootstrap/providers.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    App\Providers\AppServiceProvider::class,
+    Yajra\DataTables\DataTablesServiceProvider::class,
+    Spatie\Permission\PermissionServiceProvider::class,
+
+];
---
+++ resources/views/layouts/SIPEKA/master.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Sistem Informasi Penataan Kawasan | SIPEKA</title>
+
+	<!-- Global stylesheets -->
+	<link href="{{asset('assets/fonts/inter/inter.css')}}" rel="stylesheet" type="text/css">
+	<link href="{{asset('assets/icons/phosphor/styles.min.css')}}" rel="stylesheet" type="text/css">
+	<link href="{{asset('assets/core/assets/css/ltr/all.min.css')}}" id="stylesheet" rel="stylesheet" type="text/css">
+	<!-- /global stylesheets -->
+
+	<!-- Core JS files -->
+	<script src="{{asset('assets/demo/demo_configurator.js')}}"></script>
+	<script src="{{asset('assets/js/bootstrap/bootstrap.bundle.min.js')}}"></script>
+	<!-- /core JS files -->
+
+	<!-- Theme JS files -->
+	<script src="{{asset('assets/js/vendor/visualization/d3/d3.min.js')}}"></script>
+	<script src="{{asset('assets/js/vendor/visualization/d3/d3_tooltip.js')}}"></script>
+
+	<script src="{{asset('assets/core/assets/js/app.js')}}"></script>
+	<script src="{{asset('assets/demo/pages/dashboard.js')}}"></script>
+	{{-- <script src="{{asset('assets/demo/charts/pages/dashboard/streamgraph.js')}}"></script>
+	<script src="{{asset('assets/demo/charts/pages/dashboard/sparklines.js')}}"></script>
+	<script src="{{asset('assets/demo/charts/pages/dashboard/lines.js')}}"></script>
+	<script src="{{asset('assets/demo/charts/pages/dashboard/areas.js')}}"></script>
+	<script src="{{asset('assets/demo/charts/pages/dashboard/donuts.js')}}"></script>
+	<script src="{{asset('assets/demo/charts/pages/dashboard/bars.js')}}"></script>
+	<script src="{{asset('assets/demo/charts/pages/dashboard/progress.js')}}"></script>
+	<script src="{{asset('assets/demo/charts/pages/dashboard/heatmaps.js')}}"></script>
+	<script src="{{asset('assets/demo/charts/pages/dashboard/pies.js')}}"></script>
+	<script src="{{asset('assets/demo/charts/pages/dashboard/bullets.js')}}"></script> --}}
+	<!-- /theme JS files -->
+
+</head>
+
+<body>
+
+	<!-- Main navbar -->
+	@include('layouts.SIPEKA.navbar')
+	<!-- /main navbar -->
+
+
+	<!-- Navigation -->
+    @include('layouts.SIPEKA.navigation')
+	<!-- /navigation -->
+
+
+	<!-- Page header -->
+    @include('layouts.SIPEKA.pageheader')
+	<!-- /page header -->
+
+
+	<!-- Page content -->
+	<div class="page-content pt-0">
+
+		<!-- Main content -->
+		<div class="content-wrapper">
+
+			<!-- Content area -->
+			<div class="content">
+
+                @yield('content')
+
+			</div>
+			<!-- /content area -->
+
+		</div>
+		<!-- /main content -->
+
+	</div>
+	<!-- /page content -->
+
+
+	<!-- Footer -->
+    @include('layouts.SIPEKA.footer')
+	<!-- /footer -->
+
+
+	<!-- Notifications -->
+    @include('layouts.SIPEKA.notification')
+	<!-- /notifications -->
+
+
+	<!-- Demo config -->
+    @include('layouts.SIPEKA.config')
+	<!-- /demo config -->
+
+</body>
+</html>
